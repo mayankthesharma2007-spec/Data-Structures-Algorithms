e778427d388ff12467bc1c51da42bafa58bc5319
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-06 08:47:57 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.86%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0){
-            return false;
-        }
-        if(n==536870912){
+        if(n==1){
             return true;
         }
-        else{
-            if(log(n)/log(2) - trunc(log(n)/log(2))==0){
-                return true;
-            }
-            else{
-                return false;
-            }
+        else if(n<=0 || n%2!=0){
+            return false;
         }
-        return {};
+        return isPowerOfTwo(n/2);
     }
 };

b566525d131388c0707e9b6cb3df50731212b492
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-10 18:35:11 +0530

    Time: 0 ms (100%), Space: 23.7 MB (64.81%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,13 +1,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> answer;
-        unordered_map<int, int> mpp;
+        vector<int> answer;                                  // This is done using Moore's Voting Algorithm.
+        int cnt1 = 0, cnt2=0;
+        int el1 = INT_MIN, el2=INT_MIN;
         for(int i = 0;i<nums.size();i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]==(nums.size()/3)+1){
-                answer.push_back(nums[i]);
+            if(cnt1==0 && nums[i]!=el2){
+                cnt1=1;
+                el1=nums[i];
             }
+            else if(cnt2==0 && nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1){
+                cnt1++;
+            }
+            else if(nums[i]==el2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1=0,cnt2=0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]==el1){
+                cnt1++;
+            }
+            if(nums[i]==el2){
+                cnt2++;
+            }
+        }
+        if(cnt1>(nums.size()/3)){
+            answer.push_back(el1);
+        }
+        if(cnt2>(nums.size()/3)){
+            answer.push_back(el2);
         }
         return answer;
     }

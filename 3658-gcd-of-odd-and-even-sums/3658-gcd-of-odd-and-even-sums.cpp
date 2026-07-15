6353e5035afa328be455a91f0461f7aac7b0bc19
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-15 21:39:22 +0530

    Time: 3 ms (16.66%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -13,24 +13,10 @@ public:
         }
         int N1= max(evensum,oddsum);
         int N2= min(evensum,oddsum);
-        if(N1==0){
-            return N2;
-        }
-        else if(N2==0){
-            return N1;
-        }
         while(N1!=0 && N2!=0){
-            N1=max(N1,N2);
-            N2=min(N1,N2);
             int temp=N1%N2;
             N1=N2;
             N2=temp;
-            if(N1==1){
-                return N1;
-            }
-            else if(N2==1){
-                return N2;
-            }
         }
         return N1;
     }

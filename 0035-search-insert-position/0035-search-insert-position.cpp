daf2dfe2b4bec4271d971992a0e5d6bed69b267b
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-21 12:54:08 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.38%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,35 +1,6 @@
 class Solution {
 public:
-    int  BinarySearch(vector <int> &array, int k){
-        int low = 0;
-        int high = array.size()-1;
-        while(low <=high){
-            int mid = (low+high)/2;
-            if(array[mid]==k){
-                return mid;
-            }
-            else if(array[mid]>k){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        return BinarySearch(array,k+1);
-    }
     int searchInsert(vector<int>& nums, int target) {
-        if(target <= nums[0]){
-            return 0;
-        }
-        else if(target > nums[nums.size()-1]){
-            return nums.size();
-        }
-        else if(target == nums[nums.size()-1]){
-            return nums.size()-1;
-        }
-        else{
-            return BinarySearch(nums,target);
-        }
-        return {};
+        return lower_bound(nums.begin(),nums.end(),target)-nums.begin();  
     }
 };

340d97efc30d8a1b0821d8f1cfaa9859d6662979
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-22 12:20:01 +0530

    Time: 0 ms (100%), Space: 15.2 MB (36.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-    int BinarySearch(vector<int> &array,int k,int low , int high){
+    int BinarySearch(vector<int> &array,int k){
+        int low = 0;
+        int high = array.size()-1;
         while(low<=high){
         int mid = (low + high)/2;
         if(array[mid]==k){
             return mid;
         }
-        else if(array[mid]>k){
-            high = mid-1;
+        else if(array[low]<=array[mid]){
+            if(array[low]<= k && array[mid]>=k){
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
         else{
-            low=mid+1;
+            if(array[mid]<= k && array[high]>=k){
+                low=mid+1;
+            }
+            else{
+                high = mid-1;
+            }
         }
-        }
+    }
         return -1;
     }
     int search(vector<int>& nums, int target) {
-        int partition=-1;
-        for(int i = 0; i<nums.size()-1;i++){
-            if(nums[i+1]<nums[i]){
-                partition=i;
-                break;
-            }
-            if(nums[i+1]>nums[i] && i==nums.size()-2){
-                partition=nums.size()-1;
-            }
-        }
-        int ans1 = BinarySearch(nums,target,0,partition);
-        int ans2 = BinarySearch(nums,target,partition+1,nums.size()-1);
-        if(ans1 == -1 && ans2 == -1){
-            return -1;
-        }
-        else{
-            if(ans1==-1){
-                return ans2;
-            }
-            else if(ans2==-1){
-                return ans1;
-            }
-        }
-        return {};
+        return BinarySearch(nums,target);
     }
 };

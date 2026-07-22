a36fdd06b659d0da73c7b6ea103a285f320b06d1
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-22 13:22:10 +0530

    Time: 0 ms (100%), Space: 18.1 MB (7.67%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    bool BinarySearch(vector<int> &array, int k){
-        int low=0;
-        int high=array.size()-1;
+    int BinarySearch(vector<int> &array,int k){
+        int low = 0;
+        int high = array.size()-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(array[mid]==k){
-                return true;
-            }
-            else if(array[mid]>k){
-                high=mid-1;
+        int mid = (low + high)/2;
+        if(array[mid]==k){
+            return true;
+        }
+        else if(array[low]==array[mid] && array[mid] == array[high]){
+            low++;
+            high--;
+            continue;
+        }
+        if(array[low]<=array[mid]){
+            if(array[low]<= k && array[mid]>=k){
+                high = mid-1;
             }
             else{
                 low=mid+1;
             }
         }
+        else{
+            if(array[mid]<= k && array[high]>=k){
+                low=mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+    }
         return false;
     }
     bool search(vector<int>& nums, int target) {
-        int partition=-1;
-        for(int i = 1;i<nums.size();i++){
-            if(nums[i-1]>nums[i]){
-                partition=i-1;
-                break;
-            }
-        }
-        if(partition==-1){
-            return BinarySearch(nums,target);
-        }
-        else{
-            reverse(nums.begin(),nums.begin()+partition+1);
-            reverse(nums.begin()+partition+1,nums.end());
-            reverse(nums.begin(),nums.end());
-        }
         return BinarySearch(nums,target);
     }
 };

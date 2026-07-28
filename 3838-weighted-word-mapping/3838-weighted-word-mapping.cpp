c8e868c39c8c9db1b795f6aaa777a5a369acbff1
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-28 22:45:04 +0530

    Time: 6 ms (8.77%), Space: 45.4 MB (5.36%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        unordered_map<char,int> mpp;
-        for(int i = 0;i<weights.size();i++){
-            mpp[char(97+i)]=weights[i];
-        }
         string answer;
         for(int i = 0;i<words.size();i++){
             int sum=0;
             for(int j = 0;j<words[i].size();j++){
-                sum+=mpp[words[i][j]];
+                sum+=weights[int(words[i][j])-int('a')];
             }
             sum = sum%26;
             answer=answer+char(122-sum);

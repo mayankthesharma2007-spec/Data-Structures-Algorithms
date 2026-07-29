3b95eeed448e98e805898179fd60ae91de30355a
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-30 00:17:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.98%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        if(x==1){
+            return 1;
+        }
+        int low = 1;
+        int high = x;
+        int answer = 0;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(mid<= x/mid){
+                answer=max(answer,mid);
+                low=mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return answer;
+    }
+};

e80df9b19ea702a2ee0fb5a8446656967f3c46c1
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-07-29 22:58:36 +0530

    Time: 0 ms (100%), Space: 10 MB (11.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,19 +4,16 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        vector <int> vs;
-        for(int i = 0; i < s.size(); i++){
-            vs.emplace_back(s[i]);
+        unordered_map<char,int> mpp;
+        for(int i = 0;i<s.size();i++){
+            mpp[s[i]]++;
         }
-        sort(vs.begin(),vs.end()); 
-        vector <int> vt;
-        for(int i = 0; i < t.size(); i++){
-            vt.emplace_back(t[i]);
-        }
-        sort(vt.begin(),vt.end());
-        for(int i = 0; i < vs.size(); i++){
-            if(vs[i]!=vt[i]){
+        for(int i = 0;i<t.size();i++){
+            if(mpp[t[i]]==0){
                 return false;
+            }
+            else{
+                mpp[t[i]]--;
             }
         }
         return true;

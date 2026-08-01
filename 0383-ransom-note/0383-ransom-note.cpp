ffe38cf25124752767d10ecceae3f8695672768d
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-08-01 20:49:48 +0530

    Time: 8 ms (14.52%), Space: 11.8 MB (49.67%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char , int> mpp1;
+        for(int i = 0;i<magazine.size();i++){
+            mpp1[magazine[i]]++;
+        }
+        for(int i = 0;i<ransomNote.size();i++){
+            if(mpp1[ransomNote[i]]==0){
+                return false;
+            }
+            else{
+                mpp1[ransomNote[i]]--;
+            }
+        }
+        return true;
+    }
+};

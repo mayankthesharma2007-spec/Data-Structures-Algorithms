8ab8b46e396efa53ab7c2d36cd469091089d0666
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-08-26 23:53:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (82.87%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        int low = 0, high = 0, count = 0;
+        int low = 0;
+        int high = 0;
+        int count = 0;
         string answer = "";
-
-        while (high < s.size()) {
-            if (s[high] == '1') {
+        while(high<s.size()){
+            if(s[high]=='1'){
                 count++;
             }
-
-            while (count == k) {
-                string current = s.substr(low, high - low + 1);
-                
-                if (answer.empty() || current.length() < answer.length() || 
-                   (current.length() == answer.length() && current < answer)) {
+            while(count==k){
+                string current = s.substr(low,high-low+1);
+                if(answer.empty()||answer.length()>current.length()|| current.length()==answer.length() && answer>current){
                     answer = current;
                 }
-
-                if (s[low] == '1') {
+                if(s[low]=='1'){
                     count--;
                 }
                 low++;
             }
-
             high++;
         }
         return answer;

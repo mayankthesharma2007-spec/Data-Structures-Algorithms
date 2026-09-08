e6c04ed6c0fb83494348fa0473dacdc13e7260f4
Mayank Sharma <mayankthesharma2007@gmail.com>
2026-09-08 22:34:27 +0530

    Time: 62 ms (7.30%) | Memory: 75.7 MB (19.57%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findMiddle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* mergeLL(ListNode* leftHead, ListNode* rightHead){
+        ListNode* t1 = leftHead;
+        ListNode* t2 = rightHead;
+        ListNode* newNode = new ListNode(-1);
+        ListNode* temp = newNode;
+        while(t1!=NULL && t2!=NULL){
+            if(t1->val >= t2->val){
+                temp->next = t2;
+                t2=t2->next;
+                temp=temp->next;
+            }
+            else if(t1->val < t2->val){
+                temp->next = t1;
+                t1=t1->next;
+                temp=temp->next;
+            }
+        }
+        while(t1!=NULL){
+            temp->next = t1;
+            t1=t1->next;
+            temp=temp->next;
+        }
+        while(t2!=NULL){
+            temp->next = t2;
+            t2=t2->next;
+            temp=temp->next;
+        }
+        return newNode->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* middle = findMiddle(head);
+        ListNode* leftHead = head;
+        ListNode* rightHead = middle->next;
+        middle->next=NULL;
+        leftHead=sortList(leftHead);
+        rightHead=sortList(rightHead);
+        return mergeLL(leftHead,rightHead);
+    }
+};
